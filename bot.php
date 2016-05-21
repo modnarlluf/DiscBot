ffba6efc7d840606cf96806430ee4233f90f8df1
Brice Sigura <brice.sigura@gmail.com>
2016-05-21 16:21:43 +0200

    Removed some useless stuff

--- bot.php
+++ bot.php
@@ -1,14 +1,11 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
-define('APP_PATH', __DIR__ . '/app');
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-if (!file_exists(APP_PATH . '/config.php')) {
+if (!file_exists('./app/config.php')) {
     echo('Error, unable to fetch configuration file.'.PHP_EOL);
     exit(1);
 }
 
-$config = require_once APP_PATH . '/config.php';
+$config = require_once './app/config.php';
 
 $server = (new \ModnarLluf\DiscBot\Server($config))->run();
